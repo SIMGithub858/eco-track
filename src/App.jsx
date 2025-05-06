a4388744a593f6b583f52d94c5cc4c8db03f21e7
Théo Souza <135135018+SIMGithub858@users.noreply.github.com>
2025-05-05 23:51:08 -0300

    upd2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"; 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "/src/components/Navbar.jsx";
 
 function App() {
   return (
